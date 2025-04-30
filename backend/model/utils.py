e38b876f08c07ca59a0ca1dbeb309d50ed936ac0
SabehaKhan <sabehakhan321@gmail.com>
2025-04-29 22:34:21 -0400

    Update requirements.txt

--- backend/model/utils.py
+++ backend/model/utils.py
@@ -3,19 +3,10 @@ import django
 from safety_report.models import ArrestData
 import pandas as pd
 import numpy as np
-from math import radians, sin, cos, sqrt, atan2
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')  # Replace 'server' with your project name
 django.setup()
 
-
-def haversine(lat1, lon1, lat2, lon2):
-    R = 6371  # Radius of the Earth in kilometers
-    dlat = radians(lat2 - lat1)
-    dlon = radians(lon2 - lon1)
-    a = sin(dlat / 2) ** 2 + cos(radians(lat1)) * cos(radians(lat2)) * sin(dlon / 2) ** 2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    return R * c  # Distance in kilometers
 
 def get_crime_data():
     # Query the database and convert it to a Pandas DataFrame
